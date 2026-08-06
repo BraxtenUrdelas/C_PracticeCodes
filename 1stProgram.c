3c5455bb376bab8596be353e34d814b722950ba6
Braxten Orline Urdelas <braxtenorlineurdelas@gmail.com>
2026-08-06 10:30:48 +0800

    Add files via upload

---
+++ 1stProgram.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("This is my first C program.\n");
+    printf("I am learning C programming.\n");
+    printf("C is a powerful programming language.\n");
+    printf("I will continue to practice and improve my skills.\t");
+    return 0;
+}
