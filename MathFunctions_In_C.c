b8f25c5efa14cdf67291a0f6e5ed22fd20e8d3ba
Braxten Orline Urdelas <braxtenorlineurdelas@gmail.com>
2026-08-07 14:35:14 +0800

    Add files via upload

---
+++ MathFunctions_In_C.c
@@ -0,0 +1,25 @@
+// MATH FUNCTIONS
+
+#include <stdio.h>
+#include <math.h>   // This is needed for math functions
+
+int main() {
+
+    float x = 45;
+    //x = sqrt(x);
+    //x = pow(x, 2);
+    //x = round(x);
+    //x = ceil(x);
+    //x = floor(x);
+    //x  = abs(x);
+    //x = log(x);
+    //x = sin(x);
+    //x = cos(x);
+    //x = tan(x);
+
+
+    printf("%.2f", x);
+
+    return 0;
+
+}
